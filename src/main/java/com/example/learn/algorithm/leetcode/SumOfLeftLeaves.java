0415b1de9b6d0990b9d70c24ea6d5acf21f2941f
程凯 <kai.cheng@di-matrix.com>
2022-09-28 22:25:20 +0800

    左叶子之和

--- src/main/java/com/example/learn/algorithm/leetcode/SumOfLeftLeaves.java
+++ src/main/java/com/example/learn/algorithm/leetcode/SumOfLeftLeaves.java
@@ -1,5 +1,9 @@
 package com.example.learn.algorithm.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @description: 左叶子之和
  * <p>
@@ -26,6 +30,59 @@ public class SumOfLeftLeaves {
         }
         // 中
         int sum = midValue + leftValue + rightValue;
+
+        return sum;
+    }
+
+    public int solution2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        int result = 0;
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null && node.left.left == null && node.left.right == null) {
+                result += node.left.val;
+            }
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+
+        return result;
+    }
+
+    public int solution3(TreeNode root) {
+        int sum = 0;
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                // 左节点不为空
+                if (node.left != null) {
+                    queue.offer(node.left);
+                    // 左叶子节点
+                    if (node.left.left == null && node.left.right == null) {
+                        sum += node.left.val;
+                    }
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
 
         return sum;
     }
