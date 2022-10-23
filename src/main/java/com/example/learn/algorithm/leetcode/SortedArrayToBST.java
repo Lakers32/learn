a1da0edf9b0cb488f561312d6fe06d3d6450d303
Lakers32 <Lakers32@users.noreply.github.com>
2022-10-23 20:47:55 +0800

    将有序数组转换为二叉搜索树

---
+++ src/main/java/com/example/learn/algorithm/leetcode/SortedArrayToBST.java
@@ -0,0 +1,125 @@
+package com.example.learn.algorithm.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 将有序数组转换为二叉搜索树
+ * <p>
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SortedArrayToBST {
+
+    public TreeNode solution(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length);
+    }
+
+    /**
+     * 递归: 左闭右开 [left,right)
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if (left >= right) {
+            return null;
+        }
+        if (right - left == 1) {
+            return new TreeNode(nums[left]);
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, left, mid);
+        root.right = sortedArrayToBST(nums, mid + 1, right);
+        return root;
+    }
+
+
+    public TreeNode solution2(int[] nums) {
+        TreeNode root = traversal(nums, 0, nums.length - 1);
+        return root;
+    }
+
+    /**
+     * 左闭右闭区间[left, right)
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private TreeNode traversal(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = left + ((right - left) >> 1);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = traversal(nums, left, mid - 1);
+        root.right = traversal(nums, mid + 1, right);
+        return root;
+    }
+
+    /**
+     * 迭代: 左闭右闭 [left,right]
+     *
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+
+        //根节点初始化
+        TreeNode root = new TreeNode(-1);
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<Integer> leftQueue = new LinkedList<>();
+        Queue<Integer> rightQueue = new LinkedList<>();
+
+        // 根节点入队列
+        nodeQueue.offer(root);
+        // 0为左区间下标初始位置
+        leftQueue.offer(0);
+        // nums.size() - 1为右区间下标初始位置
+        rightQueue.offer(nums.length - 1);
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode currNode = nodeQueue.poll();
+            int left = leftQueue.poll();
+            int right = rightQueue.poll();
+            int mid = left + ((right - left) >> 1);
+
+            // 将mid对应的元素给中间节点
+            currNode.val = nums[mid];
+
+            // 处理左区间
+            if (left <= mid - 1) {
+                currNode.left = new TreeNode(-1);
+                nodeQueue.offer(currNode.left);
+                leftQueue.offer(left);
+                rightQueue.offer(mid - 1);
+            }
+
+            // 处理右区间
+            if (right >= mid + 1) {
+                currNode.right = new TreeNode(-1);
+                nodeQueue.offer(currNode.right);
+                leftQueue.offer(mid + 1);
+                rightQueue.offer(right);
+            }
+        }
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {-9, -7, -4, 0, 1, 3, 8};
+
+        SortedArrayToBST sortedArrayToBST = new SortedArrayToBST();
+        System.out.println("Sorted Array To BST " + sortedArrayToBST.solution(nums));
+    }
+}
